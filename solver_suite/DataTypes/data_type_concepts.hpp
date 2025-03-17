78331b945c7cef366902ad718aa8be5928665844
MiguelVeganzones <veganzones28@gmail.com>
2025-03-17 11:49:29 +0100

    Improved concepts

--- solver_suite/DataTypes/data_type_concepts.hpp
+++ solver_suite/DataTypes/data_type_concepts.hpp
@@ -1,34 +1,39 @@
 #pragma once
 
 #include "concepts.hpp"
+#include <concepts>
 #include <iterator>
+#include <type_traits>
 #include <utility>
 
 namespace data_types::dt_concepts
 {
 
 template <typename T>
-concept StaticArray = requires(T t) {
-    T::size;
-    std::begin(t);
-    std::end(t);
-};
-
-template <typename T>
-concept DynamicArray = requires(T t, const T ct, typename T::size_type n) {
-    { ct.size() } -> std::same_as<typename T::size_type>;
-    { t.resize(n) } -> std::same_as<void>;
-    { std::begin(t) } -> std::input_or_output_iterator;
-    { std::end(t) } -> std::input_or_output_iterator;
-};
-
-template <typename T>
 concept SizedInstance = requires(T t) { t.size(); };
 
 template <typename T>
-concept Resizeable = requires(T t, typename T::size_type n) {
-    t.size();
+concept Resizeable = requires(T t, const T ct, typename T::size_type n) {
+    ct.size();
     t.resize(n);
+};
+
+template <typename T>
+concept Indexable = requires(T t) {
+    { t[std::declval<int>()] };
+};
+
+template <typename T>
+concept Indexable2D = requires(T t) {
+    { t[std::declval<int>(), std::declval<int>()] };
+};
+
+struct dynamic_array_base
+{
+};
+
+struct data_buffer_base
+{
 };
 
 struct expression_templates_base
@@ -36,25 +41,41 @@ struct expression_templates_base
 };
 
 template <typename T>
-concept ExpressionTemplate = std::is_base_of_v<expression_templates_base, T>;
+concept StaticArray =
+    requires { T::s_size; } && Indexable<T> && SizedInstance<T> && std::ranges::range<T>;
+
+template <typename T>
+concept DynamicArray = std::is_base_of_v<dynamic_array_base, T> && Indexable<T> &&
+                       SizedInstance<T> && Resizeable<T> && std::ranges::range<T>;
+
+template <typename T>
+concept Buffer = std::is_base_of_v<data_buffer_base, T> && Indexable<T> &&
+                 Indexable2D<T> && SizedInstance<T> && std::ranges::range<T>;
+
+template <typename T>
+concept ExpressionTemplate =
+    std::is_base_of_v<expression_templates_base, T> && Indexable<T>;
 
 template <typename T>
 concept ScalarType = utility::concepts::arithmetic<T>;
 
 template <typename T>
-concept ValidExprOperand =
-    DynamicArray<T> || ExpressionTemplate<T> || ScalarType<T> || StaticArray<T>;
+concept ValidExprOperand = DynamicArray<T> || ExpressionTemplate<T> || ScalarType<T> ||
+                           StaticArray<T> || Buffer<T>;
 
 template <typename T>
 concept EagerEvaluation = StaticArray<T> || ScalarType<T>;
 
 template <typename T>
-concept LazyEvaluation = ExpressionTemplate<T> || DynamicArray<T>;
+concept LazyEvaluation = ExpressionTemplate<T> || DynamicArray<T> || Buffer<T>;
 
 template <typename A, typename B>
 concept EagerlyEvaluatedExpr = EagerEvaluation<A> && EagerEvaluation<B>;
 
 template <typename A, typename B>
 concept LazilyEvaluatedExpr = LazyEvaluation<A> || LazyEvaluation<B>;
+
+template <typename T>
+concept ExprHoldReferenceType = DynamicArray<T> || Buffer<T>;
 
 } // namespace data_types::dt_concepts
